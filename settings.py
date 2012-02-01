ed0b0ee0939fb6c2ecf643531cb423b56920e040
Rob McQueen <robmcqn@gmail.com>
2012-01-31 19:47:46 -0800

    adding local settings configuration

--- settings.py
+++ settings.py
@@ -43,20 +43,10 @@ USE_I18N = True
 # calendars according to the current locale
 USE_L10N = True
 
-# Absolute filesystem path to the directory that will hold user-uploaded files.
-# Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = '/Users/rob/Documents/myprojects/djangoprojects/foos/media/'
-
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
 MEDIA_URL = '/media/'
-
-# Absolute path to the directory static files should be collected to.
-# Don't put anything in this directory yourself; store your static files
-# in apps' "static/" subdirectories and in STATICFILES_DIRS.
-# Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = '/Users/rob/Documents/myprojects/djangoprojects/foos/mystatic/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -147,3 +137,7 @@ LOGGING = {
 
 AUTH_PROFILE_MODULE = "main.UserProfile"
 
+try:
+    from local_settings import *
+except ImportError:
+    pass
